e599868b793c9fd6beb65288464a3da6e1c1b86c
unknown <taylordarnell89@yahoo.com>
2019-02-21 15:08:36 -0800

    make it better

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -260,14 +260,14 @@ class ResponsiveDrawer extends React.Component {
                       <b> Mission-driven front end developer with a passion for thoughtful UI design,
                 collaboration, and learning. </b>
                     </p>
-                    <p class='text textResponsive'>
+                    <p class='text textResponsive3'>
                       I’ve always sought out opportunities and challenges that are meaningful to me.
                       Although my professional path has taken many twists and turns
                        — from World Traveling Musician to Sr. Account Executive
                        — I've never stopped engaging my passion to help others and solve problems.
                  </p>
 
-                    <p class='text textResponsive'>
+                    <p class='text textResponsive3'>
                       As a web developer, I enjoy using my obsessive attention to detail, my unequivocal love for making things,
                       and my mission-driven work ethic to bring the best effort to any challenge presented.
                       That's why I’m excited to make a big impact at a high growth company.
